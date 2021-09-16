08dd5a8bd0be74099c64da68cc9ac655225a32b3
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-16 22:04:00 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/hasht/HashTableMain.java
@@ -0,0 +1,7 @@
+package com.bdlz.hasht;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the HashTable Program");
+    }
+}
---
+++ src/test/java/com/bdlz/hasht/HashTableMainTest.java
@@ -0,0 +1,14 @@
+package com.bdlz.hasht;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashTableMainTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
